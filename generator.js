7ce9531890034f533fde35e2cb7c2d4ade5f5319
ravi-aratchige <rmaratchige@gmail.com>
2022-10-26 01:23:01 +0530

    Added clipboard function
    Doesn't work yet.

--- generator.js
+++ generator.js
@@ -76,5 +76,19 @@ function generatePassword (upper,lower,number,symbol,length) {
     	return finalPassword;
     }
    
+// RAVINDU'S CODE STARTS HERE
 
-
+function clipboard() {
+    // Get the text field
+    var copyText = document.getElementById("result");
+    
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+    
+        // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
